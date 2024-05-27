0038ce0c5883897a7816afea173544616dabcbe7
Bastien <bureaubastien3@gmail.com>
2024-05-27 12:35:51 +0200

    Add : Test unitaire concernant les catégories

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,28 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import "dart:math";
+
+import "package:flutter/material.dart";
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flymenu/main.dart';
+
+import "package:flymenu/Model/restaurant.dart";
+import "package:flymenu/Model/menu.dart";
+import "package:flymenu/Model/categorie.dart";
+import "package:flymenu/Model/product.dart";
+import "package:flymenu/Services/categories_services.dart";
+
+const String DRINK = 'Drinks';
+const String FOODS = 'Foods';
+
+List<Categorie> categories = [
+  Categorie(name: 'Starters'),
+  Categorie(name: FOODS),
+  Categorie(name: 'Desserts'),
+  Categorie(name: DRINK),
+];
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -27,4 +45,62 @@ void main() {
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
+
+  List<Product> products = [];
+  Random random = Random();
+  for (int i = 0; i < 20; i++) {
+    products.add(
+      Product(
+        title: 'Product ${i + 1}',
+        description: 'Description for product ${i + 1}',
+        price: 5.0 + random.nextDouble() * 20.0,
+        imagePath: 'assets/product_${random.nextInt(5) + 1}.jpg',
+        categorie: categories[random.nextInt(categories.length)],
+        isHappyHour: random.nextBool()
+      ),
+    );
+  }
+
+  List<FoodMenu> menus = [];
+  for (int i = 0; i < 10; i++) {
+    menus.add(
+      FoodMenu(
+        menuCategories: categories,
+        products: products,
+      ),
+    );
+  }
+
+  List<Restaurant> restaurants = [];
+  restaurants.add(
+      Restaurant(
+          name: 'Restaurant le petit gibier',
+          address: '124 chemin de la croisade',
+          isOpen: true,
+          menus: menus
+      )
+  );
+
+  Restaurant currentRestaurant = restaurants.first;
+  FoodMenu currentMenu = currentRestaurant.menus.first;
+  
+  group("Test Menu filter", () {
+    test('Test that a list of products contains only drinks', () {
+      // Obtention de la liste des boissons à partir de la liste de tous les produits
+      List<Product> drinks = CategoriesService.filterByCategories(currentMenu, DRINK);
+
+      // Vérification que la liste de boissons ne contient que des boissons
+      expect(drinks.every((product) => product.categorie.name == DRINK), isTrue);
+    });
+
+    test('Test that a list of products contains only food', () {
+      // Obtention de la liste des boissons à partir de la liste de tous les produits
+      List<Product> foods = CategoriesService.filterByCategories(currentMenu, FOODS);
+
+      // Vérification que la liste de boissons ne contient que des boissons
+      expect(foods.every((product) => product.categorie.name == FOODS), isTrue);
+    });
+  });
+
+  
 }
