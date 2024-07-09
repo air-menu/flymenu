f8272b85ec4713940b1f615f642ac9c6e8a00877
Bastien <bureaubastien3@gmail.com>
2024-07-09 14:38:33 +0200

    Revert "Merge suit : Implements tracking on Model categorie and product"
    This reverts commit 7ffab3af69ed4b37739e0994c21e9ba8b32eb510.

---
+++ lib/model/categorie.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flymenu/Model/selectable.dart';
+import 'package:flymenu/services/categories_services.dart';
+
+class Categorie extends Selectable {
+
+  String? id;
+
+  Categorie({this.id, required String name}) {
+    super.name = name;
+  }
+
+  factory Categorie.fromFirestore(DocumentSnapshot doc, {bool isFromObject = false}) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+    if(isFromObject){
+      DocumentReference result = data['categorie'];
+      return CategoriesService().categories.collection.firstWhere((categorie) => categorie.id == result.id);
+    }
+    else{
+      return Categorie(id: data['id'], name: data['name']);
+    }
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name
+    };
+  }
+}
